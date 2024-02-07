51cbc3dfc8944f6646bea8400e2ce6df77ce0e8d
Joseph Rydel <jrydel92@gmail.com>
2024-02-06 23:01:13 -0600

    Initial commit.

---
+++ Lib/main.py
@@ -0,0 +1,118 @@
+from tkinter import *
+from spotify import *
+from tkinter import messagebox
+from datetime import datetime
+from bs4 import BeautifulSoup
+import requests
+
+
+FONT = "Arial"
+SIZE = 12
+NUM_WIDTH = 8
+PADDING = 20
+
+
+def process_date(m_text, d_text, y_text):
+    month = m_text.get()
+    day = d_text.get()
+    year = y_text.get()
+    if len(month) == 0 or len(day) == 0 or len(year) == 0:
+        messagebox.showwarning(title="Entry Error",
+                               message="All fields are required. Please try again.")
+    else:
+        try:
+            desired_date = datetime(year=int(year), month=int(month), day=int(day)).strftime("%Y-%m-%d")
+        except TypeError:
+            error_msg()
+        except ValueError:
+            error_msg()
+        else:
+            songs = get_songs(desired_date)
+            song_uris = get_song_uris(songs, year)
+            playlist_response = spotify_client.create_playlist(desired_date, song_uris)
+            window.quit()
+
+def error_msg():
+    messagebox.showwarning(title="Entry Error", message="Please enter a valid date.")
+    month_input.delete(0, END)
+    day_input.delete(0, END)
+    year_input.delete(0, END)
+    month_input.focus()
+
+def get_songs(date):
+    response = requests.get(f"https://www.billboard.com/charts/hot-100/{date}")
+    soup = BeautifulSoup(response.text, "html.parser")
+    first_song = soup.find(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s "
+                                             "u-letter-spacing-0021 u-font-size-23@tablet "
+                                             "lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max "
+                                             "a-truncate-ellipsis u-max-width-245 u-max-width-230@tablet-only "
+                                             "u-letter-spacing-0028@tablet").getText().strip("\n\t")
+
+    song_titles = soup.find_all(name="h3", class_="c-title a-no-trucate a-font-primary-bold-s "
+                                          "u-letter-spacing-0021 lrv-u-font-size-18@tablet "
+                                          "lrv-u-font-size-16 u-line-height-125 u-line-height-normal@mobile-max "
+                                          "a-truncate-ellipsis u-max-width-330 u-max-width-230@tablet-only")
+
+    songs = [song.getText().strip('\n\t') for song in song_titles]
+    songs.insert(0, first_song)
+    return songs
+
+def get_song_uris(songs, year):
+    song_uris = []
+    for song in songs:
+        try:
+            response = spotify_client.spotify_search(song, year)
+            uri = response["tracks"]["items"][0]["uri"]
+        except ValueError as error:
+            print(error)
+        except TypeError as error:
+            print(error)
+        except IndexError:
+            print("Song not available. Not added.")
+        except ConnectionError as error:
+            print(error)
+        except requests.exceptions.HTTPError as http_error:
+            print(http_error)
+        else:
+            song_uris.append(uri)
+    return song_uris
+
+
+spotify_client = SpotifyDataOperations()
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Enter Date")
+window.config(padx=PADDING, pady=PADDING)
+my_canvas = Canvas(width=100, height=100, highlightthickness=0)
+window.configure(background='light grey')
+prompt_label = Label(text="What particular time are you nostalgic for? Enter the date.", font=(FONT, SIZE))
+prompt_label.config(padx=PADDING, pady=PADDING)
+prompt_label.configure(background='light grey')
+
+month_label = Label(text="Month:", font=(FONT, SIZE))
+month_label.configure(background='light grey')
+day_label = Label(text="Day:", font=(FONT, SIZE))
+day_label.configure(background='light grey')
+month_label.configure(background='light grey')
+year_label = Label(text="Year:", font=(FONT, SIZE))
+year_label.configure(background='light grey')
+
+month_input = Entry(width=NUM_WIDTH)
+day_input = Entry(width=NUM_WIDTH)
+year_input = Entry(width=12)
+
+enter = Button(text="Enter", width=10, command=lambda: process_date(month_input, day_input, year_input))
+
+prompt_label.grid(column=1, row=1, columnspan=6)
+month_label.grid(column=1, row=2, pady=PADDING)
+month_input.grid(column=2, row=2, pady=PADDING, sticky=W)
+day_label.grid(column=3, row=2, pady=PADDING)
+day_input.grid(column=4, row=2, pady=PADDING, sticky=W)
+year_label.grid(column=5, row=2, pady=PADDING)
+year_input.grid(column=6, row=2, pady=PADDING, sticky=W)
+
+enter.grid(column=2, row=3, columnspan=4)
+
+window.mainloop()
---
+++ Lib/spotify.py
@@ -0,0 +1,63 @@
+import os
+import spotipy
+import requests
+from spotipy.oauth2 import SpotifyOAuth
+
+#This class is responsible for handling all data related to Spotify and communicating with the Spotify API.
+
+class SpotifyDataOperations:
+    def __init__(self):
+        self.client_id = os.environ.get("CLIENT_ID")
+        self.client_secret = os.environ.get("CLIENT_SECRET")
+        self.scope = 'playlist-modify-private'
+        self.auth_manager = SpotifyOAuth(client_id=self.client_id,
+            client_secret=self.client_secret,
+                                         scope=self.scope,
+                                         redirect_uri="http://example.com")
+        self.access_token = self.auth_manager.get_access_token()['access_token']
+        self.endpoint = "https://api.spotify.com/v1/"
+
+        self.header = {
+            "Authorization": f"Bearer {self.access_token}"
+        }
+
+        self.client = spotipy.Spotify(auth_manager=self.auth_manager)
+        self.user = self.client.current_user()['id']
+
+
+    def spotify_search(self, song, year):
+        response = self.client.search(
+            q=f"{song},{year}", type="track", market="US", offset=0)
+        return response
+
+    def create_playlist(self, date, uri_list):
+        playlist_create_endpoint = self.endpoint + f"users/{self.user}/playlists"
+        playlist_params = self.get_playlist_params(date)
+
+        my_playlist = requests.post(url=playlist_create_endpoint, json=playlist_params, headers=self.header)
+        my_playlist_json = my_playlist.json()
+
+        my_playlist_id = my_playlist_json['id']
+        playlist_add_endpoint = self.endpoint + f"playlists/{my_playlist_id}/tracks"
+
+        playlist_add_params = self.get_playlist_add_params(uri_list)
+
+        playlist_reponse = requests.post(
+            url=playlist_add_endpoint, json=playlist_add_params, headers=self.header)
+        return playlist_reponse
+
+
+    def get_playlist_params(self, date):
+        parameters = {
+            "name": f"{date} Billboard Top 100",
+            "description": f"Billboard Top 100 songs in {date}.",
+            "public": False
+        }
+        return parameters
+
+    def get_playlist_add_params(self, uri_list):
+        parameters = {
+            "uris": uri_list,
+            "position": 0
+        }
+        return parameters
